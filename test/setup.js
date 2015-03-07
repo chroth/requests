d19e367bfa7f9bf1b3389cb3574b11efec9d704b
Christofer Roth <christofer@dorian.se>
2015-03-07 10:52:49 +0100

    Removed node support

--- test/setup.js
+++
@@ -1,6 +0,0 @@
-// Export modules to global scope as necessary (only for testing)
-if (typeof process !== 'undefined' && process.title === 'node') {
-  isBrowser = false;
-} else {
-  isBrowser = true;
-}
